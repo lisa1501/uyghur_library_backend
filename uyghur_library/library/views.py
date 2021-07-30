ba95c450e85d20019e5cebcb1b9e36e86711cd20
halisa <halisasaipulaa@gmail.com>
2021-07-30 14:19:52 -0700

    make Book model make migrations

--- uyghur_library/library/views.py
+++ uyghur_library/library/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Book
 # from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'library/home.html')
+    context = {
+        'books': Book.objects.all()
+    }
+    return render(request, 'library/home.html', context)
+
 
 def about(request):
-    return render(request, 'library/about.html')
+    return render(request, 'library/about.html', {'title': 'About'})
